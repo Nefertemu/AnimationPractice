d8e81c4d395bb6dcc0cb3a0241cda291da178985
Bogdan Anishchenkov <bogdaggi@gmail.com>
2022-09-13 19:00:08 +0300

    Almost done

--- AnimationPractice/ViewController.swift
+++ AnimationPractice/ViewController.swift
@@ -12,6 +12,8 @@ final class ViewController: UIViewController {
     
     let animations = Animation.getAnimations()
     
+    var currentAnimation = 0
+    
     @IBOutlet var springAnimationView: SpringView!
 
     @IBOutlet var presetLabel: UILabel!
@@ -22,15 +24,41 @@ final class ViewController: UIViewController {
     
     @IBOutlet var runButton: SpringButton!
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        runButton.backgroundColor = .darkGray
+        
+        springAnimationView.layer.cornerRadius = 30
+        runButton.layer.cornerRadius = 15
+        
+        setupAnimationView()
     }
 
     @IBAction func runButtonPressed(_ sender: SpringButton) {
-        springAnimationView.animation
+        springAnimationView.animation = animations[currentAnimation].preset.rawValue
+        springAnimationView.curve = animations[currentAnimation].curve.rawValue
+        springAnimationView.force = animations[currentAnimation].force
+        springAnimationView.duration = animations[currentAnimation].duration
+        springAnimationView.delay = animations[currentAnimation].delay
         springAnimationView.animate()
+        
+        setupAnimationView()
+    }
+    
+    private func setupAnimationView() {
+        
+        if currentAnimation <= animations.count {
+            presetLabel.text = "preset: " + animations[currentAnimation].preset.rawValue
+            curveLabel.text = "curve: \(animations[currentAnimation].curve.rawValue)"
+            forceLabel.text = "force: " + String(format: "%.1f", animations[currentAnimation].force)
+            durationLabel.text = "duration: " + String(format: "%.1f", animations[currentAnimation].duration)
+            delaylabel.text = "delay: " + String(format: "%.1f", animations[currentAnimation].delay)
+            
+            runButton.titleLabel?.text = "Run " + animations[currentAnimation + 1].preset.rawValue
+            
+            currentAnimation += 1
+        }
     }
     
 }
