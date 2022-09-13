9db3e408c16e68868648371c4ac5a651b4f1471d
Bogdan Anishchenkov <bogdaggi@gmail.com>
2022-09-13 12:46:09 +0300

    Initial Commit

---
+++ AnimationPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimationPractice
+//
+//  Created by Bogdan Anishchenkov on 13.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
